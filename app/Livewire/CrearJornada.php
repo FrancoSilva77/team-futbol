82e70e1814162eda841f94b626266015d0865c4d
Franco Silva <franco_tigres18@hotmail.com>
2024-06-07 19:12:51 -0600

    feat(Jornada):Validar datos

--- app/Livewire/CrearJornada.php
+++ app/Livewire/CrearJornada.php
@@ -8,6 +8,27 @@ use Livewire\Component;
 class CrearJornada extends Component
 {
 
+    public $jornada;
+    public $equipo_contrario;
+    public $color;
+    public $goles_favor;
+    public $goles_contra;
+    public $fecha;
+
+    protected $rules = [
+        'jornada' => 'required|integer',
+        'equipo_contrario' => 'required|string',
+        'color' => 'required',
+        'goles_favor' => 'required|integer',
+        'goles_contra' => 'required|integer',
+        'fecha' => 'required|string'
+    ];
+
+    public function crearVacante()
+    {
+        $data = $this->validate();
+    }
+
     public function render()
     {
         $colores = Color::all();
--- resources/views/livewire/crear-jornada.blade.php
+++ resources/views/livewire/crear-jornada.blade.php
@@ -1,13 +1,14 @@
-<form action="" class="md:w-1/3 space-y-5">
+<form class="md:w-1/3 space-y-5" wire:submit.prevent='crearVacante'>
     <div>
         <x-input-label for="jornada" :value="__('Número de Jornada')" />
-        <x-text-input id="jornada" class="block mt-1 w-full" type="number" name="jornada" :value="old('jornada')" autofocus />
+        <x-text-input id="jornada" class="block mt-1 w-full" type="number" wire:model="jornada" :value="old('jornada')"
+            autofocus />
         <x-input-error :messages="$errors->get('jornada')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="equipo_contrario" :value="__('Nombre del Equipo Contrario')" />
-        <x-text-input id="equipo_contrario" class="block mt-1 w-full" type="text" name="equipo_contrario"
+        <x-text-input id="equipo_contrario" class="block mt-1 w-full" type="text" wire:model="equipo_contrario"
             :value="old('equipo_contrario')" />
         <x-input-error :messages="$errors->get('equipo_contrario')" class="mt-2" />
     </div>
@@ -15,7 +16,7 @@
     <div>
         <x-input-label for="color" :value="__('Vestimenta del equipo contrario (Color Dominante)')" />
 
-        <select name="color" id="color"
+        <select wire:model="color" id="color"
             class="border-gray-300 focus:border-green-500 focus:ring-green-500 rounded-md shadow-sm w-full">
             <option value="">-- Seleccione --</option>
 
@@ -29,20 +30,21 @@
 
     <div>
         <x-input-label for="goles_favor" :value="__('Resultado (Goles a Favor)')" />
-        <x-text-input id="goles_favor" class="block mt-1 w-full" type="number" name="goles_favor" :value="old('goles_favor')" />
+        <x-text-input id="goles_favor" class="block mt-1 w-full" type="number" wire:model="goles_favor"
+            :value="old('goles_favor')" />
         <x-input-error :messages="$errors->get('goles_favor')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="goles_contra" :value="__('Resultado (Goles en Contra)')" />
-        <x-text-input id="goles_contra" class="block mt-1 w-full" type="number" name="goles_contra"
+        <x-text-input id="goles_contra" class="block mt-1 w-full" type="number" wire:model="goles_contra"
             :value="old('goles_contra')" />
         <x-input-error :messages="$errors->get('goles_contra')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="fecha" :value="__('Fecha de la jornada')" />
-        <x-text-input id="fecha" class="block mt-1 w-full" type="date" name="fecha" :value="old('fecha')" />
+        <x-text-input id="fecha" class="block mt-1 w-full" type="date" wire:model="fecha" :value="old('fecha')" />
         <x-input-error :messages="$errors->get('fecha')" class="mt-2" />
     </div>
 
