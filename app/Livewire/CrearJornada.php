2c0cdd9f53adbb56a9f5411b39124db7edf63b94
Franco Silva <franco_tigres18@hotmail.com>
2024-06-07 18:34:41 -0600

    feat(Form crear-jornada)

---
+++ app/Livewire/CrearJornada.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CrearJornada extends Component
+{
+    public function render()
+    {
+        return view('livewire.crear-jornada');
+    }
+}
